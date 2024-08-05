b88e4e618b9022caa6a01da88b85914f57c8983d
saintleech <saintleech@proton.me>
2024-08-05 20:04:33 +0200

    Adding type definitions for Movement

--- Main.c
+++ Main.c
@@ -1,8 +1,28 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+typedef enum MovementType
+{
+  PROFIT = 0,
+  LOSS,
+} MovementType;
+
+typedef struct Movement
+{
+  MovementType type;
+  float amount;
+  struct Movement* next;
+} Movement;
+
 int main()
 {
-  printf("Expenc\n");
+  Movement movement =
+  {
+    .type = PROFIT,
+    .amount = 10.f,
+    .next = NULL,
+  };
+  printf("%d %f %p\n", movement.type, movement.amount, movement.next);
+
   return EXIT_SUCCESS;
 }
